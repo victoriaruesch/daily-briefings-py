42ff979b9ed979acacc4c91031b8d644cc3aa35f
Victoria Ruesch <77935587+victoriaruesch@users.noreply.github.com>
2021-04-12 17:35:12 -0400

    Setup flask quickstart example

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/newpath")
+def new_route():
+    return "New Path!"
